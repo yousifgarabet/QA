b48afe8c73955349ab8a35c1d7b926d13d4a3abd
yousifgarabet <yousifgarabet@gmail.com>
2020-03-05 01:53:03 +0300

    creating question form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -25,7 +25,9 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create', \compact('question'));
     }
 
     /**
