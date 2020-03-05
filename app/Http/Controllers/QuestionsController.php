0045ed88ab3dc7b1fa940c9cd5dd7373b77a628a
yousifgarabet <yousifgarabet@gmail.com>
2020-03-06 02:19:25 +0300

    autherization using policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -9,6 +9,11 @@ use Illuminate\Auth\Access;
 
 class QuestionsController extends Controller
 {
+    // constructor
+    public function __construct()
+    {
+            $this->middleware('auth',['except'=> ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -69,9 +74,13 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if (\Gate::denies('update-question', $question)) {
-            abort(403,'Access Denied');
-        }
+        //   autherizing using gate
+        // if (\Gate::denies('update-question', $question)) {
+        //     abort(403,'Access Denied');
+        // }
+
+        // Autherizing using policy
+        $this->authorize('update', $question);
         return view('questions.edit', \compact('question'));
     }
 
@@ -84,9 +93,14 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if (\Gate::denies('update-question', $question)) {
-            abort(403,'Access Denied');
-        }
+        //  autherizing using gate
+        // if (\Gate::denies('update-question', $question)) {
+        //     abort(403,'Access Denied');
+        // }
+
+                // Autherizing using policy
+        $this->authorize('update', $question);
+
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success','The question has been updated');
 
@@ -100,9 +114,13 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if (\Gate::denies('delete-question', $question)) {
-            abort(403,'Access Denied');
-        }
+        //  autherizing using gate
+        // if (\Gate::denies('delete-question', $question)) {
+        //     abort(403,'Access Denied');
+        // }
+
+                // Autherizing using policy
+        $this->authorize('delete ', $question);
         $question->delete();
         return redirect('/questions')->with('success','Your question has been deleted');
     }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -34,16 +34,16 @@
                                      <h3 class='mt-0'> <a  href="{{$question->url}}" > {{$question->title}} </a></h3>
                                      <div class='ml-auto'>
                                          <!--  to check if user is signed in or not -->
-                                         @if(Auth::user()->can('update-question', $question))
+                                         @can('update', $question)
                                             <a href="{{route('questions.edit', $question->id)}}" class='btn btn-sm btn-outline-info'>Edit</a>
-                                         @endif
+                                         @endcan
 
                                          <form class='form-delete' method="POST" action="{{route('questions.destroy', $question->id)}}">
                                            @method('DELETE')
                                            @csrf
-                                           @if(Auth::user()->can('delete-question', $question))
+                                           @can('delete', $question)
                                                  <button type='submit' class='btn btn-sm btn-outline-danger' onclick="return confirm('Are you sure?')">Delete</button>
-                                           @endif
+                                           @endcan
                                        </form>
                                      </div>
                                 </div>
