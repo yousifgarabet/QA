41711e375d073bb1cd5105ee648ac5ce791e4c66
yousifgarabet <yousifgarabet@gmail.com>
2020-03-05 15:00:26 +0300

    Delete question

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\users\yousi\desktop\qa\qa\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\users\yousi\desktop\qa\qa\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\users\yousi\Desktop\QA\QA\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\users\yousi\Desktop\QA\QA\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
