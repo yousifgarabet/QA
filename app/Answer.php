50801aa137ab69e8a2da88ab2475e1e3816b7c35
yousifgarabet <yousifgarabet@gmail.com>
2020-03-06 17:03:05 +0300

    showing the answers for each question

--- app/Answer.php
+++ app/Answer.php
@@ -31,4 +31,8 @@ class Answer extends Model
             $answer->question->save();
          });
      }
+     public function getCreatedDateAttribute()
+     {
+         return $this->created_at->diffForHumans();
+     }
 }
