9cc4d3591a702e684f79b0afb92e3ad54f9ac788
yousifgarabet <yousifgarabet@gmail.com>
2020-03-15 15:38:03 +0300

    using elequent to update best_answer_id when removing it

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\This;
 
 class Answer extends Model
 {
@@ -43,5 +44,9 @@ class Answer extends Model
      public function getCreatedDateAttribute()
      {
          return $this->created_at->diffForHumans();
+     }
+     public function getStatusAttribute()
+     {
+         return $this->id ==$this->question->best_answer_id? 'vote-accepted': '';
      }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,7 +17,7 @@
                                     <a title="this answer is not useful" class="vote-down off">
                                     <i class="fas fa-caret-down fa-3x"></i>
                                     </a>
-                                    <a title="mark this answer as the best answer" class="vote-accepted mt-2">
+                                    <a title="mark this answer as the best answer" class="{{$answer->status}} mt-2">
                                     <i class="fas fa-check fa-2x"></i>
                                     </a>
                             </div>
